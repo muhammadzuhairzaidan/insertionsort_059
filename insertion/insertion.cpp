e2f228444fdaeb9b0a13d7e839db0d752306cf53
Muhammad Zuhair Zaidan <muhammad.zuhair.ft22@mail.umy.ac.id>
2023-03-24 16:56:31 +0700

    menambahkan prosedur display dengan tipe data void

--- insertion/insertion.cpp
+++ insertion/insertion.cpp
@@ -24,4 +24,35 @@ void input() {
 
 	for (int i = 0; i < n; i++)
 	{
+		cout << "data ke-" << (i + 1) << ":";
+		cin >> arr[i];
+	}
+}
+
+void insertionsort() {
+	int temp;
+	int j;
+
+	for (int i = 1; i < n; i++) {
+
+		temp = arr[i];
+
+		j = i - 1;
+
+		while (j >= 0 && arr[j] > temp)
+
+		{
+			arr[j + 1] = arr[j];
+			j--;
+		}
+		arr[j + 1] = temp;
+
+		cout << "\nPass " << i << ":";
+		for (int k = 0; k < n; k++) {
+			cout << arr[k] << "";
+		}
+	}
+}
+
+void display() {
 	
