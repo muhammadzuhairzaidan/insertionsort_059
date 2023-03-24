cd8bc1139fc07591ff3f755e264cd4b13237d3a7
Muhammad Zuhair Zaidan <muhammad.zuhair.ft22@mail.umy.ac.id>
2023-03-24 16:54:41 +0700

    melakukan pengulangan i dengan for

--- insertion/insertion.cpp
+++ insertion/insertion.cpp
@@ -7,4 +7,21 @@ int n;
 void input() {
 	while (true) {
 		cout << "masukan jumlah data pada array";
-		
+		cin >> n;
+
+		if (n <= 20) {
+			break;
+		}
+		else
+		{
+			cout << "\n Array yang anda masukan maksimal 20 elemen. \n";
+		}
+	}
+	cout << endl;
+	cout << "=====================" << endl;
+	cout << "Masukan Element Array" << endl;
+	cout << "=====================" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+	
